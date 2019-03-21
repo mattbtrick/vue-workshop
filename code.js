f566f10c98ffec484bf730e81ab72b51d39d19df
Matthew Bronkema <mbronkema@quantros.com>
2019-03-20 22:35:35 -0400

    Step 1

--- code.js
+++ code.js
@@ -2,13 +2,19 @@ window.vm = new Vue({
     el: '#app',
     data: function() {
         return {
-            heading: 'To Do List'
+            heading: 'To Do List',
+            tasks: []
         };
     },
     created: function() {
-
+        this.getTasks();
     },
     methods: {
-
+        getTasks: function() {
+            var self = this;
+            api.getList(function(tasks) {
+                self.tasks = tasks;
+            });
+        }
     }
 });
